ed75850c1bd53068ab6ff76e3362d903bd2e803a
matheusel <matheuselh@gmail.com>
2025-04-11 17:58:34 -0300

    remove redundant instructions and streamline validation steps in instrucoes.py

--- web/components/instrucoes.py
+++ web/components/instrucoes.py
@@ -4,10 +4,6 @@ def instrucoes_validar_importacoes():
     st.markdown("---")
     st.info("""
 
-    ###### ℹ️ **Este módulo verifica se o arquivo está formatado corretamente e se há imagem no osinfo.**
-
-    ⚠️ **Se ocorrer algum erro em qualquer etapa, reporte ao desenvolvedor responsável e capture uma imagem (print) antes de atualizar a página.**
-
     ### **✅ Passo a Passo**
 
     1️⃣ **Selecione** o tipo de arquivo/módulo.
@@ -23,14 +19,9 @@ def instrucoes_validar_alteracoes():
     st.markdown("---")
     st.info("""
 
-    ###### ℹ️ **Este módulo valida se há imagem no osinfo.**
-
-    ⚠️ **Se ocorrer algum erro em qualquer etapa, reporte ao desenvolvedor responsável e capture uma imagem (print) antes de atualizar a página.**
-
     ### **✅ Passo a Passo**
 
     1️⃣ **Verifique** se o **ATRIBUTO** está de acordo com os padrões definidos para o **TIPO_MODULO**.  
-       - Modelos disponíveis: [📂 Google Drive](https://drive.google.com/drive/u/1/folders/18iWis2JHzRyYJPcFu6vBRhbPyFzpAl6C)
 
     2️⃣ **Selecione** a instituição.
 
