2426de9fd8fb75c7f08fb83eb81035513232a4af
matheusel <matheuselh@gmail.com>
2026-08-11 17:30:57 -0300

    Atualiza a validação de campos no BaseValidatorIns para ignorar 'CONTRATO_ANO_MES_INICIO' e 'CONTRATO_ANO_MES_FIM', melhorando a precisão na verificação de dados.

--- models/base_validador.py
+++ models/base_validador.py
@@ -58,8 +58,8 @@ class BaseValidatorIns(ABC):
             return
             
         for campo in self.datas_abreviadas:
-            if campo in ['REF_TRI']:
-                pass
+            if campo in ['CONTRATO_ANO_MES_INICIO', 'CONTRATO_ANO_MES_FIM']:
+                continue
             if campo in self.df.columns:
                 valores_unicos = self.df[campo].unique()
                 if len(valores_unicos) > 1:
